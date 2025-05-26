e92cda176e342b89b077a340bb795b89ff829f2c
voluch <voluch364@gmail.com>
2025-05-26 11:38:59 +0300

    Added new metrics

--- metrics.py
+++ metrics.py
@@ -13,3 +13,53 @@ class MeanSquaredError:
         """
         mse = np.mean((original_signal - denoised_signal) ** 2)
         return mse
+
+
+class MeanAbsoluteError:
+    @staticmethod
+    def calculate(original_signal, denoised_signal):
+        """
+        Обчислює середню абсолютну помилку (MAE) між оригінальним і очищеним сигналами.
+
+        :param original_signal: Оригінальний сигнал (1D масив).
+        :param denoised_signal: Очищений сигнал (1D масив).
+        :return: Значення MAE.
+        """
+        mae = np.mean(np.abs(original_signal - denoised_signal))
+        return mae
+
+
+class RootMeanSquaredError:
+    @staticmethod
+    def calculate(original_signal, denoised_signal):
+        """
+        Обчислює корінь середньоквадратичної помилки (RMSE) між оригінальним і очищеним сигналами.
+
+        :param original_signal: Оригінальний сигнал (1D масив).
+        :param denoised_signal: Очищений сигнал (1D масив).
+        :return: Значення RMSE.
+        """
+        rmse = np.sqrt(np.mean((original_signal - denoised_signal) ** 2))
+        return rmse
+
+
+class SignalToNoiseRatio:
+    @staticmethod
+    def calculate(original_signal, denoised_signal):
+        """
+        Обчислює співвідношення сигнал/шум (SNR) між оригінальним і очищеним сигналами в децибелах (dB).
+
+        :param original_signal: Оригінальний сигнал (1D масив).
+        :param denoised_signal: Очищений сигнал (1D масив).
+        :return: Значення SNR у dB.
+        """
+        noise = original_signal - denoised_signal
+        signal_power = np.sum(original_signal ** 2)
+        noise_power = np.sum(noise ** 2)
+
+        # Захист від ділення на нуль
+        if noise_power == 0:
+            return np.inf
+
+        snr = 10 * np.log10(signal_power / noise_power)
+        return snr
